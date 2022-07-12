6298f640a0b24aefd7d2e18c9b8417ac415ec65d
jimmyblackwood576@outlook.com <jimmyblackwood576@outlook.com>
2022-07-12 22:49:09 +0300

    Добавьте файлы проекта.

---
+++ Work_arrays/Work_arrays.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    
+    int arr[3][4], num;
+    cout << "Enter the number: \n";
+    cin >> num;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 4; j++) {
+            arr[i][j] = num;
+            num *= 2;
+        }
+    }
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 4; j++) {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
